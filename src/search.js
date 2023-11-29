34423202e24cae6e4c9bc0c2d98422dcefc18bb3
kellhusCorp <kellhus.corp@gmail.com>
2023-11-30 00:07:36 +0400

    Added regexp processing

--- src/search.js
+++ src/search.js
@@ -1,15 +1,24 @@
 export default (docs, term) => {
     const results = [];
     const getWords = (s) => {
-        return s.split(' ').map(x => x.toLowerCase());
+        return s.split(' ').map(x => getTerm(x));
     }
+
+    const exp = /\w+/g;
+
+    const getTerm = (token) => {
+        const matches = token.match(exp);
+        return matches ? matches[0].toLowerCase() : '';
+    }
+
+    const prepTerm = getTerm(term);
 
     for (const doc of docs) {
 
         const words = getWords(doc.text);
 
         for (const word of words) {
-            if (word === term) {
+            if (word === prepTerm) {
                 if (results.indexOf(doc.id) === -1) {
                     results.push(doc.id);
                     break;
